ba8fb6bc90ddd9961937f2083130787340d26fdc
hariprathap-hp <hariprathap89@gmail.com>
2022-04-03 16:39:56 +0530

    code for listing urls updated

--- domain/tinyurl/url_dao.go
+++ domain/tinyurl/url_dao.go
@@ -3,17 +3,14 @@ package tinyurl
 import (
 	"fmt"
 	"strconv"
-	"test3/hariprathap-hp/system_design/TinyURL/dataResources/postgresDB/urls_db"
 	"test3/hariprathap-hp/system_design/tinyURL/dataResources/cassandra"
-	"test3/hariprathap-hp/system_design/tinyURL/logger"
 	"test3/hariprathap-hp/system_design/tinyURL/utils/errors"
+	"time"
 )
 
 const (
-	indexUniqueUserID  = "duplicate key value"
 	queryinsertTinyURL = "INSERT into urls (tiny_url, original_url, creation_date, expiration_date, user_id) values (?,?,?,?,?)"
-	querylistAllURLs   = "select "
-	searchQuery        = " hash,originalurl,creationdate,expirationdate from url where user_id=$1"
+	querylistAllURLs   = "select tiny_url, original_url, creation_date, expiration_date from urls where user_id=? ALLOW FILTERING"
 	querydeleteTinyURL = "delete from urls where user_id=? and original_url=?"
 )
 
@@ -26,32 +23,28 @@ func (url *Url) Save() *errors.RestErr {
 }
 
 func (url *Url) List() (Urls, *errors.RestErr) {
-	stmt, err := urls_db.Client.Prepare(searchQuery)
-	if err != nil {
-		logger.Error("error while trying to create db statement", err)
-		return nil, errors.NewInternalServerError("databse error")
-	}
-	defer stmt.Close()
+
 	user_id, _ := strconv.Atoi(url.UserID)
+	fmt.Println("user_id list is --", user_id)
 
-	rows, searchErr := stmt.Query(user_id)
-	if searchErr != nil {
-		fmt.Println(searchErr)
-		return nil, errors.NewInternalServerError("fetching users from database failed")
+	var results Urls
+	m := map[string]interface{}{}
+
+	iter := cassandra.GetSession().Query(querylistAllURLs, user_id).Iter()
+
+	for iter.MapScan(m) {
+		results = append(results, Url{
+			TinyURL:        m["tiny_url"].(string),
+			OriginalURL:    m["original_url"].(string),
+			CreationDate:   m["creation_date"].(time.Time),
+			ExpirationDate: m["expiration_date"].(time.Time),
+		})
+		m = map[string]interface{}{}
 	}
-	defer rows.Close()
-
-	results := make([]Url, 0)
-	for rows.Next() {
-		var res Url
-		scanErr := rows.Scan(&res.TinyURL, &res.OriginalURL,
-			&res.CreationDate, &res.ExpirationDate)
-		if scanErr != nil {
-			return nil, errors.NewInternalServerError("failed during scanning result rows")
-		}
-		results = append(results, res)
+	if err := iter.Close(); err != nil {
+		fmt.Println(err.Error())
+		return nil, errors.NewInternalServerError(err.Error())
 	}
-
 	return results, nil
 }
 
